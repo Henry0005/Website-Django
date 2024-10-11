37a9ef3539333a92b8bcb11602ad95bce493473b
Henry0005 <103142324+Henry0005@users.noreply.github.com>
2024-10-11 14:31:54 -0300

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Usuario(models.Model):
-    nome = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    data_criacao = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.nome
